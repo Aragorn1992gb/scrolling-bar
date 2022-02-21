19dda685453263d0119ecec109cdfb232b114565
Aragorn1992gb <giacomobrunetta@gmail.com>
2022-02-21 22:50:09 +0100

    bugfix

--- bar.js
+++ bar.js
@@ -1,100 +1,149 @@
 var flag;
 
-var barHeigth = (document.getElementById("myContainer").clientHeight);
-var textTop = (document.getElementById("text-container").offsetTop);
+    var barHeigth = (document.getElementById("containerBar").clientHeight);
+    var textTop = (document.getElementById("text-container").offsetTop);
+    var textHeight = (document.getElementById("text-container").clientHeight);
 
-var textDeep = document.getElementById("text-container").textContent.length;
-var scrollPercentage = 2;
-var pxForTimes = (textDeep * scrollPercentage) / 100;
+    var textDeep = document.getElementById("text-container-2").offsetHeight;
 
-var pointClicked = 0;
-var pxForTimesText = 0;
+    var scrollPercentage = 5;
+    var pxForTimes = (textDeep * scrollPercentage) / 100;
 
+    var n = (textDeep - textHeight) / pxForTimes + 1;
 
-var pos = 0;
-var pos2 = 0;
+	
+	n = n<=0 ? 1 : n
+    var littleBarHeight = barHeigth / n;
 
-document.getElementById("myContainer").style.top = textTop.toString() + 'px';
-document.getElementById("upButton").style.top = (textTop - 50).toString() + 'px';
-document.getElementById("downButton").style.top = (textTop + barHeigth + 25).toString() + 'px';
+    var pxForTimesText = 0;
 
-function scrollDown(pixel) {
-    document.getElementById("text-container").scrollTop += pixel;
-}
+    var pos = 0;
 
-function scrollUp(pixel) {
-    document.getElementById("text-container").scrollTop -= pixel;
-}
+    var limitStep = 0;
 
-function moveBar1(event) {
+    var contBar = document.getElementById("movingBar");
+    contBar.style.height = littleBarHeight.toString() + 'px';
 
-    var elem = document.getElementById("myAnimation");
-    var rect = elem.getBoundingClientRect();
+    var elementTxtContainer = document.getElementById('text-container');
+    var positionTextTop = elementTxtContainer.getBoundingClientRect();
+    var elementMvBar = document.getElementById("movingBar");
 
-    flag = false;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (flag) {
-            clearInterval(id);
-        } else {
-            pos += pxForTimes;
-            scrollDown(pos);
-            flag = true;
-            elem.style.top = pos + 'px';
-        }
+    document.getElementById("containerBar").style.top = textTop + "px";
+    document.getElementById("upButton").style.top = (textTop - 50).toString() + 'px';
+    document.getElementById("downButton").style.top = (textTop + barHeigth + 25).toString() + 'px';
+
+    function scrollDown(pixel) {
+      document.getElementById("text-container").scrollTop += pixel;
     }
 
-}
+    function scrollUp(pixel) {
+      document.getElementById("text-container").scrollTop -= pixel;
+    }
 
-function moveBar2(event) {
+    function moveBar1(event) {
 
-    var elem = document.getElementById("myAnimation");
+      var rect = elementMvBar.getBoundingClientRect();
+      var bbMovement = rect.bottom - textTop;
+      var btMovement = rect.top - textTop;
 
-    var rect = elem.getBoundingClientRect();
-    console.log("posizione elem " + rect.top);
-
-    flag = false;
-    var id = setInterval(frame, 10);
-    function frame() {
+      flag = false;
+      var id = setInterval(frame, 10);
+      function frame() {
         if (flag) {
-            clearInterval(id);
+          clearInterval(id);
         } else {
-            if (event.clientY > rect.bottom + 25 || event.clientY < rect.top - 25) {
+          pos += littleBarHeight;
+          scrollDown(pxForTimes);
+          flag = true;
 
-                pxForTimesText = (event.clientY * textDeep / document.getElementById('myContainer').clientHeight + (document.getElementById('myContainer').offsetTop / 2));
+          if (bbMovement >= barHeigth - littleBarHeight) {
+            pos = (barHeigth - littleBarHeight);
+            elementMvBar.style.bottom = (barHeigth) + 'px';
+          }
+          elementMvBar.style.top = pos + 'px';
+        }
+      }
+    }
 
+    function moveBar2(event) {
+      var clickedY = event.clientY - textTop;
+      var rect = elementMvBar.getBoundingClientRect();
+      var bbMovement = rect.bottom - textTop;
+      var btMovement = rect.top - textTop;
 
-                if (event.clientY >= document.getElementById('myContainer').clientHeight - (document.getElementById('myContainer').offsetTop / 2)) {
-                    pos = (document.getElementById('myContainer').clientHeight - document.getElementById('myContainer').offsetTop);
-                    alert(pos);
-                }
-
-                else {
-                    pos = (event.clientY - (document.getElementById('myContainer').offsetTop / 2)) - (document.getElementById('myAnimation').clientHeight);
-                }
-
-                document.getElementById("text-container").scrollTop = pxForTimesText;
-
-
+      flag = false;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (flag) {
+          clearInterval(id);
+        } else {
+          if ((clickedY > (bbMovement + limitStep)) || (clickedY < (btMovement - limitStep))) {
+            var jumpScrollPercent = (clickedY * 100) / barHeigth;
+            pxForTimesText = (jumpScrollPercent * (textDeep - textHeight)) / 100;
+            if (clickedY >= barHeigth - (littleBarHeight / 2)) {
+              pos = (barHeigth - littleBarHeight);
+              pxForTimesText = textDeep + textHeight;
             }
-            else if (event.clientY < rect.top) {
-                pos -= pxForTimes;
-                scrollUp(pos);
+            else if (clickedY <= littleBarHeight / 2) {
+              pos = 0;
+              pxForTimesText = 0;
             }
             else {
-                pos += pxForTimes;
-                scrollDown(pos);
+              pos = clickedY - (littleBarHeight / 2);
             }
-            flag = true;
-            elem.style.top = pos + 'px';
+            document.getElementById("text-container").scrollTop = pxForTimesText;
+          }
+          else if (clickedY < btMovement) {
+            if (clickedY <= littleBarHeight / 2) {
+              pos = 0;
+            }
+            else {
+              pos -= littleBarHeight;
+            }
+            scrollUp(pxForTimes);
+          }
+          else {
+
+            if (clickedY >= barHeigth - (littleBarHeight / 2)) {
+              pos = (barHeigth - littleBarHeight);
+            }
+            else {
+              pos += littleBarHeight;
+            }
+            scrollDown(pxForTimes);
+          }
+          flag = true;
+          elementMvBar.style.top = pos + 'px';
         }
+      }
     }
 
-}
+    function moveBar1Up(event) {
+      var rect = elementMvBar.getBoundingClientRect();
+      var bbMovement = rect.bottom - textTop;
+      var btMovement = rect.top - textTop;
+      flag = false;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (flag) {
+          clearInterval(id);
+        } else {
+          pos -= littleBarHeight;
+          scrollUp(pxForTimes);
+          flag = true;
 
-(document.getElementById("myAnimation")).addEventListener("click", moveBar1);
-(document.getElementById("myContainer")).addEventListener("click", moveBar2);
+          if (pos < (littleBarHeight / 2))
+            pos = 0;
+          elementMvBar.style.top = pos + 'px';
+        }
+      }
+    }
 
-(document.getElementById("myAnimation")).addEventListener("touchstart", moveBar1);
-(document.getElementById("myContainer")).addEventListener("touchstart", moveBar2);
-
+    (document.getElementById("movingBar")).addEventListener("click", moveBar1);
+    (document.getElementById("containerBar")).addEventListener("click", moveBar2);
+    (document.getElementById("movingBar")).addEventListener("touchstart", moveBar1);
+    (document.getElementById("containerBar")).addEventListener("touchstart", moveBar2);
+    (document.getElementById("upButton")).addEventListener("click", moveBar1Up);
+    (document.getElementById("upButton")).addEventListener("touchstart", moveBar1Up);
+    (document.getElementById("downButton")).addEventListener("click", moveBar1);
+    (document.getElementById("downButton")).addEventListener("touchstart", moveBar1);
